1ddd29ad2c4ddce2da8f2415f9f03cca82a77c55
Paulo Andrade <paulo@pandrade.info>
2017-01-31 03:07:15 +0000

    My first working Django App!

--- init_garden.sh
+++ init_garden.sh
@@ -4,3 +4,6 @@ nohup python /home/pi/git/growroom/legacy/lights.py &
 #init basic soil sensor and water pump integration
 nohup python /home/pi/git/growroom/legacy/Main.py &
 
+#initialize django webserver for growroom
+python /home/pi/git/growroom/MyGarden/manage.py runserver 192.168.69:8000
+
